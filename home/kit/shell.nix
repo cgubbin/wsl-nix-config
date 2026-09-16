b82daea421c076f7ce02a3783c79647aec714c03
Christopher Gubbin <christopher.gubbin@wavephotonics.com>
2026-09-16 12:08:46 +0000

    added fish as login

--- home/kit/shell.nix
+++ home/kit/shell.nix
@@ -1,5 +1,5 @@
 {pkgs, ...}: {
-  programs.bash.interactiveShellInit = ''
+  programs.bash.initExtra = ''
     if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" ]]; then
       exec ${pkgs.fish}/bin/fish
     fi
