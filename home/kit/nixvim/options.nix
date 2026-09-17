d7d25d8621898842fad5a6ca4fea6fe5226cc3ab
Christopher Gubbin <christopher.gubbin@wavephotonics.com>
2026-09-17 09:29:29 +0000

    corrected scm pathing

--- home/kit/nixvim/options.nix
+++ home/kit/nixvim/options.nix
@@ -9,7 +9,7 @@
       "lua/kit/functions/utils.lua".source = ./lua/kit/functions/utils.lua;
       "lua/kit/functions/init.lua".source = ./lua/kit/functions/init.lua;
       # Treesitter queries in markdown
-      "after/queries/markdown/textobjects.scm".source = ./after/queries/ecma/textobjects.scm;
+      "after/queries/markdown/textobjects.scm".source = ./after/queries/markdown/textobjects.scm;
 
       # Markdown snippets
       "LuaSnip/markdown/utils/conditions.lua".source = ./snippets/markdown/utils/conditions.lua;
